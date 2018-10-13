b853a68c538b94fe8f158cc825c21c99c7a15a7c
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-13 10:39:43 +0200

    add third

--- Documents/javastart/extra_1310/src/FIlmweb.java
+++ Documents/javastart/extra_1310/src/FIlmweb.java
@@ -1,21 +1,15 @@
 public class FIlmweb {
     public static void main(String[] args) {
         //zmienna: typ, referencje, przypisanie
-//        String title1 = "Titanic";
-//        int year = 1997;
-//        String genre = "Dramat";
-//        String director = "James Cameron";
-        //nizej:::
 
         //tworzenie obiektu
-        Movie  movie1 = new Movie();
-        movie1.title = "Titanic";
-        movie1.year = 1997;
-        movie1.director = "James Cameron";
-        movie1.genre = "Dramat";
+        Movie  movie1 = new Movie("Titanic", 1997, "James Cameron", "Dramat");
+//        movie1.title = "Titanic";
+//        movie1.year = 1997;
+//        movie1.director = "James Cameron";
+//        movie1.genre = "Dramat";
 
-        //System.out.println(title1 + " " + director + " " + genre + " " + year);
-       // System.out.printf("%s %s %s %d", movie1.title, director, genre, year);
-        //::przeniesienie do Movie jako metoda
+        movie1.show();
+        //wywowalenie metody na obiekcie
     }
 }
--- Documents/javastart/extra_1310/src/Movie.java
+++ Documents/javastart/extra_1310/src/Movie.java
@@ -7,12 +7,22 @@ public class Movie {
     String director;
     String genre;
 
+
+    //konstruktory
+    //:::utworzenie konstruktora nie(!) domyslnego
+
+    public Movie(String title, int year, String director, String genre) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+    }
+
     //metody
 
     void show() {
         System.out.printf("%s %s %s %d", title, director, genre, year);
     }
-    //konstruktory
 
 
 }
